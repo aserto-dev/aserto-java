f63a75f2a296e5cc79c1c9d8e476e55988b09518
Bogdan Irimie <bogdan.irimie@aserto.com>
2023-03-17 17:51:20 +0200

    Add decisionTree and getPolicy apis

--- src/main/java/com/aserto/AuthzClient.java
+++ src/main/java/com/aserto/AuthzClient.java
@@ -1,10 +1,9 @@
 package com.aserto;
 
 import com.aserto.authorizer.v2.*;
-import com.aserto.authorizer.v2.api.IdentityContext;
+import com.aserto.authorizer.v2.Decision;
+import com.aserto.authorizer.v2.api.*;
 import com.aserto.authorizer.v2.api.Module;
-import com.aserto.authorizer.v2.api.PolicyContext;
-import com.aserto.authorizer.v2.api.PolicyInstance;
 import com.aserto.model.IdentityCtx;
 import com.aserto.model.PolicyCtx;
 import com.google.protobuf.Struct;
@@ -26,67 +25,96 @@ public class AuthzClient implements AutoCloseable {
     public List<Module> listPolicies(String policyName, String policyLabel) {
         ListPoliciesRequest.Builder policyBuilder = ListPoliciesRequest.newBuilder();
 
-        PolicyInstance policy = getPolicy(policyName, policyLabel);
+        PolicyInstance policy = buildPolicy(policyName, policyLabel);
         policyBuilder.setPolicyInstance(policy);
 
-        ListPoliciesRequest listPoliciesRequest = policyBuilder.build();
-        ListPoliciesResponse response = client.listPolicies(listPoliciesRequest);
+        ListPoliciesResponse response = client.listPolicies(policyBuilder.build());
 
         return response.getResultList();
     }
 
-    public List<Decision> is(IdentityCtx identityContext, PolicyCtx policyContext) {
+    public Module getPolicy(String policyId) {
+        GetPolicyRequest.Builder getPolicyBuilder = GetPolicyRequest.newBuilder();
+        getPolicyBuilder.setId(policyId);
+
+        GetPolicyResponse policyResponse = client.getPolicy(getPolicyBuilder.build());
+
+        return policyResponse.getResult();
+    }
+
+    public List<Decision> is(IdentityCtx identityCtx, PolicyCtx policyCtx) {
         IsRequest.Builder isBuilder = IsRequest.newBuilder();
 
-        IdentityContext.Builder identityContextBuilder =  IdentityContext.newBuilder();
-        identityContextBuilder.setIdentity(identityContext.getIdentity());
-        identityContextBuilder.setType(identityContext.getIdentityType());
+        IdentityContext identityContext = buildIdentityContext(identityCtx);
+        PolicyContext policyContext = buildPolicyContext(policyCtx);
+        PolicyInstance policy = buildPolicy(policyCtx.getName(), policyCtx.getLabel());
 
-
-        PolicyContext.Builder policyContextBuilder = PolicyContext.newBuilder();
-        policyContextBuilder.setPath(policyContext.getPath());
-        policyContextBuilder.addAllDecisions(Arrays.asList(policyContext.getDecisions()));
-
-
-        isBuilder.setIdentityContext(identityContextBuilder.build());
-        isBuilder.setPolicyContext(policyContextBuilder.build());
-
-        PolicyInstance policy = getPolicy(policyContext.getName(), policyContext.getLabel());
+        isBuilder.setIdentityContext(identityContext);
+        isBuilder.setPolicyContext(policyContext);
         isBuilder.setPolicyInstance(policy);
 
-        IsResponse isReponse = client.is(isBuilder.build());
+        IsResponse isResponse = client.is(isBuilder.build());
 
-        return isReponse.getDecisionsList();
+        return isResponse.getDecisionsList();
     }
 
     public Struct query(String query, PolicyCtx policyContext, Map<String, Value> values) {
         QueryRequest.Builder queryRequestBuilder = QueryRequest.newBuilder();
         queryRequestBuilder.setQuery(query);
 
-        PolicyInstance policy = getPolicy(policyContext.getName(), policyContext.getLabel());
-        queryRequestBuilder.setPolicyInstance(policy);
-
-
+        PolicyInstance policy = buildPolicy(policyContext.getName(), policyContext.getLabel());
         Struct.Builder structBuilder = Struct.newBuilder();
         values.forEach(structBuilder::putFields);
+
+        queryRequestBuilder.setPolicyInstance(policy);
         queryRequestBuilder.setResourceContext(structBuilder);
 
-        QueryRequest queryRequest = queryRequestBuilder.build();
-        QueryResponse queryResponse = client.query(queryRequest);
+        QueryResponse queryResponse = client.query(queryRequestBuilder.build());
 
         return queryResponse.getResponse();
     }
 
+    public Map<String, Value> decisionTree(IdentityCtx identityCtx, PolicyCtx policyCtx){
+        DecisionTreeRequest.Builder decisionTreeBuilder = DecisionTreeRequest.newBuilder();
+
+        IdentityContext identityContext = buildIdentityContext(identityCtx);
+        PolicyContext policyContext = buildPolicyContext(policyCtx);
+        PolicyInstance policy = buildPolicy(policyCtx.getName(), policyCtx.getLabel());
+
+        decisionTreeBuilder.setIdentityContext(identityContext);
+        decisionTreeBuilder.setPolicyContext(policyContext);
+        decisionTreeBuilder.setPolicyInstance(policy);
+
+        DecisionTreeResponse decisionTree = client.decisionTree(decisionTreeBuilder.build());
+
+        return decisionTree.getPath().getFieldsMap();
+    }
 
     public void close() {
         channel.shutdown();
     }
 
-    private PolicyInstance getPolicy(String name, String label) {
+    private PolicyInstance buildPolicy(String name, String label) {
         PolicyInstance.Builder policyInstance = PolicyInstance.newBuilder();
         policyInstance.setName(name);
         policyInstance.setInstanceLabel(label);
 
         return policyInstance.build();
+    }
+
+    private IdentityContext buildIdentityContext(IdentityCtx identityContext) {
+        IdentityContext.Builder identityContextBuilder =  IdentityContext.newBuilder();
+        identityContextBuilder.setIdentity(identityContext.getIdentity());
+        identityContextBuilder.setType(identityContext.getIdentityType());
+
+        return identityContextBuilder.build();
+    }
+
+    private PolicyContext buildPolicyContext(PolicyCtx policyContext) {
+        PolicyContext.Builder policyContextBuilder = PolicyContext.newBuilder();
+        policyContextBuilder.setPath(policyContext.getPath());
+        policyContextBuilder.addAllDecisions(Arrays.asList(policyContext.getDecisions()));
+
+        return policyContextBuilder.build();
     }
 }
