7cf773bf83f016eea1c98bd414a6ea3c30aabf5c
Bogdan Irimie <bogdan.irimie@aserto.com>
2023-04-03 16:04:34 +0300

    Add small example.

---
+++ examples/authz-example/README.md
@@ -0,0 +1,65 @@
+[![slack](https://img.shields.io/badge/slack-Aserto%20Community-brightgreen)](https://asertocommunity.slack.com)
+
+
+## 0. prerequisites
+
+Ensure you are in the `examples` directory.
+
+```bash
+cd examples
+```
+
+## 1. building the examples
+
+To build the example package, execute the following commands:
+
+```bash
+mvn clean package
+```
+
+## 2. setup environment
+
+## 2.1 using [topaz](https://topaz.sh)
+
+### 2.1.1 install and configure [topaz](https://topaz.sh)
+
+* Install [topaz](https://github.com/aserto-dev/topaz#installation)
+* Configure topaz to use the `todo` policy
+
+```bash
+topaz configure -d -s -r ghcr.io/aserto-policies/policy-todo:v2 todo
+```
+
+* Download topaz directory data
+
+```bash
+topaz stop 
+wget https://raw.githubusercontent.com/aserto-dev/topaz/main/pkg/testing/assets/eds-citadel.db -O ~/.config/topaz/db/directory.db
+```
+
+* Start topaz
+
+```bash
+topaz start
+
+```
+
+* Validate if topaz is running
+
+```bash
+topaz status
+```
+
+### 2.1.2 copy the `examples/assets/.env.topaz-authorizer.example` to the `examples` directory
+
+```bash
+cp assets/.env.topaz-authorizer.example .env
+```
+
+## 3. Running the example
+
+To run the examples, execute:
+
+```bash
+java -jar target/examples-1.0.0-SNAPSHOT-shaded.jar
+```
---
+++ examples/authz-example/src/main/java/org/example/AuthzExample.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import com.aserto.AuthzClient;
+import com.aserto.ChannelBuilder;
+import com.aserto.authorizer.v2.Decision;
+import com.aserto.authorizer.v2.api.IdentityType;
+import com.aserto.model.IdentityCtx;
+import com.aserto.model.PolicyCtx;
+import io.grpc.ManagedChannel;
+
+import javax.net.ssl.SSLException;
+import java.util.List;
+
+public class AuthzExample {
+    public static void main(String[] args) throws SSLException {
+        // create a channel that has the connection details
+        ManagedChannel channel = new ChannelBuilder()
+                .withAddr("localhost:8282")
+                .withInsecure(true)
+                .build();
+
+        // create authz client
+        AuthzClient authzClient =  new AuthzClient(channel);
+
+        // identity context contains information abou the user that requests access to some resource
+        IdentityCtx identityCtx = new IdentityCtx("rick@the-citadel.com", IdentityType.IDENTITY_TYPE_SUB);
+
+        // contains information about the policy we want to check for the provided identity
+        PolicyCtx policyCtx = new PolicyCtx("todo", "todo", "todoApp.DELETE.todos.__id", new String[]{"allowed"});
+
+        // check if the identity is allowed to perform the action
+        List<Decision> decisions = authzClient.is(identityCtx, policyCtx);
+        authzClient.close();
+
+        decisions.forEach(decision -> {
+            String dec = decision.getDecision();
+            boolean isAllowed =  decision.getIs();
+            System.out.println("For decision [" + dec + "] the answer was [" + isAllowed + "]");
+        });
+    }
+}
