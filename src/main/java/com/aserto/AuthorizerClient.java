cbcfbc8d279930cbe37a66793a654c0c086058e7
Bogdan Irimie <bogdan.irimie@aserto.com>
2023-05-11 11:34:30 +0300

    Extend AuthorizerClient interface with is call that accept resource context

--- src/main/java/com/aserto/AuthorizerClient.java
+++ src/main/java/com/aserto/AuthorizerClient.java
@@ -14,6 +14,7 @@ public interface AuthorizerClient extends AutoCloseable {
     public List<Module> listPolicies(String policyName, String policyLabel);
     public Module getPolicy(String policyId);
     public List<Decision> is(IdentityCtx identityCtx, PolicyCtx policyCtx);
+    public List<Decision> is(IdentityCtx identityCtx, PolicyCtx policyCtx, Map<String, Value> resourceCtx);
     public Struct query(String query, PolicyCtx policyContext, Map<String, Value> values);
     public Map<String, Value> decisionTree(IdentityCtx identityCtx, PolicyCtx policyCtx);
 }
